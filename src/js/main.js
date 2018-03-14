53434dec0e3999ce1b9fee592a0e554dd454c7d6
howardpanton <howardpanton@gmail.com>
2018-03-14 22:38:42 +0000

    refactor: add template literal to restaurant list

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,14 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  DBHelper.onWindowResize();
   fetchNeighborhoods();
   fetchCuisines();
   registerSW();
+});
+
+window.addEventListener("resize", (event) => {
+  DBHelper.onWindowResize();
 });
 
 /**
@@ -128,7 +133,8 @@ const resetRestaurants = (restaurants) => {
 const fillRestaurantsHTML = (restaurants = self.restaurants) => {
   const ul = document.getElementById('restaurants-list');
   restaurants.forEach(restaurant => {
-    ul.append(createRestaurantHTML(restaurant));
+    // ul.appendChild(createRestaurantHTML(restaurant));
+    ul.insertAdjacentHTML('beforeend', createRestaurantHTML(restaurant));
   });
   addMarkersToMap();
 }
@@ -137,31 +143,38 @@ const fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 const createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+  const li = `
+    <li>
+      <figure>
+        ​<picture>
+        <source srcset="${DBHelper.imageUrlForRestaurant(restaurant)
+          .replace(/\.webp$/, "-lrg-desktop.webp")}"
+          media="(min-width: 1024px)">
+        <source srcset="${DBHelper.imageUrlForRestaurant(restaurant)
+          .replace(/\.webp$/, "-desktop.webp")}"
+          media="(min-width: 768px)">
+        <source srcset="${DBHelper.imageUrlForRestaurant(restaurant)
+          .replace(/\.webp$/, "-mobile-l.webp")}"
+          media="(min-width: 425px)">
+        <source srcset="${DBHelper.imageUrlForRestaurant(restaurant)
+          .replace(/\.webp$/, "-mobile-m.webp")}"
+          media="(min-width: 375px)">
+        <source srcset="${DBHelper.imageUrlForRestaurant(restaurant)
+          .replace(/\.webp$/, "-mobile-s.webp")}"
+          media="(min-width: 320px)">
+        <img class="restaurant-img" alt="${restaurant.alt}"
+        src="${DBHelper.imageUrlForRestaurant(restaurant)
+          .replace(/\.webp$/, "-lrg-desktop.webp")}" >
+        </picture>
+      </figure>
+      <h1>${restaurant.name}</h1>
+      <p>${restaurant.neighborhood}</p>
+      <p>${restaurant.address}</p>
+      <a href="${DBHelper.urlForRestaurant(restaurant)}">View Details</a>
+    </li>
+  `;
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
-
-  const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
-  li.append(name);
-
-  const neighborhood = document.createElement('p');
-  neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
-
-  const address = document.createElement('p');
-  address.innerHTML = restaurant.address;
-  li.append(address);
-
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
-
-  return li
+  return li;
 }
 
 /**
